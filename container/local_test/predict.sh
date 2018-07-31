cb863a49ddb2f772efa8398fb0d65c839b6459ce
kirk.kalvar@kal.technology <kalvar01>
2018-07-31 12:21:23 -0400

    Change curl flag to -i

--- container/local_test/predict.sh
+++ container/local_test/predict.sh
@@ -3,4 +3,5 @@
 payload=$1
 content=${2:-image/png}
 
-curl --connect-timeout 600 --data-binary @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
+#curl --data-binary @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
+curl --data-binary @${payload} -H "Content-Type: ${content}" -i http://localhost:8080/invocations
