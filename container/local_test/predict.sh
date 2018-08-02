872c52938ddc406ce186b2c7c61f4bcbeabb910a
kirk.kalvar@kal.technology <kalvar01>
2018-08-02 10:47:50 -0400

    Going back to using text/csv

--- container/local_test/predict.sh
+++ container/local_test/predict.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 payload=$1
-content=${2:-image/png}
+content=${2:-text/csv}
 
 #curl --data-binary @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
 curl --data-binary @${payload} -H "Content-Type: ${content}" -i http://localhost:8080/invocations
+echo
